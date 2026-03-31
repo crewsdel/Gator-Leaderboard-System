64ae67c4cdf1b7eddc1e6bdcb3f3c213bb66c90d
crewsdel <124068095+crewsdel@users.noreply.github.com>
2026-03-31 12:16:22 -0400

    Create AVLTree.py

---
+++ AVLTree.py
@@ -0,0 +1,22 @@
+# This file defines the AVL Tree data structure
+
+class Node:
+
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 0
+        self.size = 1
+
+
+
+
+class AVLTree:
+
+    def __init__(self, root):
+        self.key = None
+
+
+
+
