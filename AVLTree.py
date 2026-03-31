0ae524425fec281344d2a0dae386de4e9721d286
crewsdel <124068095+crewsdel@users.noreply.github.com>
2026-03-31 13:03:20 -0400

    Update AVLTree.py

--- AVLTree.py
+++ AVLTree.py
@@ -17,6 +17,46 @@ class AVLTree:
     def __init__(self):
         self.root = None
 
+    # Inserts a player into the tree
+    def insert(self, score, player_id):
+        # 1. If tree is empty → create root
+        # 2. Otherwise, → call recursive insert
+        pass
+
+    # Helper for inserting player
+    def _insert(self, node, score, player_id):
+        pass
+        # 1. If node is None → create new Node
+        # 2. Compare (score, player_id) with node.key
+        #    - if higher rank → go left
+        #    - else → go right
+        # 3. Update height
+        # 4. Update size
+        # 5. Check balance
+        # 6. Perform rotations if needed
+
+    def delete(self, player_id):
+        # 1. If player_id not in the tree, output "Player <player_id> not found
+        # 2. Find score from the hash table
+        # 3. Delete(score, player_id) from the tree and hash table, no output required
+        pass
+
+    def _delete(self, node, score, player_id):
+        # 1. If node is None
+        # 2. Compare key with node.key
+        #   - If target should be left, recurse left
+        #   - If target should be right, recurse right
+        #   - else, found the node to delete
+        # 3. Once found, handle the 3 BST delete cases:
+        #   - no children
+        #   - one child
+        #   - two children
+        # 4. After deletion:
+        #   - update heigh, size, compute balance, rebalance with rotations if necessary
+        pass
+
+
+
 
 
 
